1a6c08510bd3edae55857ae7131f72214f04eb1e
Oleh <85799629+olehmatskiv@users.noreply.github.com>
2023-01-29 12:06:28 +0200

    add js and foto , style

---
+++ js/main.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    $('.slider').slick({
+        arrows:true,
+        dots:true,
+        adaptiveHeight:true,
+        slidesToScroll:1,
+        slidesToShow:1,
+        speed:2000,
+        easing:'ease',
+        infinite:true,
+        initialSlide:0,
+        autoplay:true,
+        autoplaySpeed:3000,
+        pauseOnFocus:true,
+        pauseOnHover:true,
+        pauseOnDotsHover:true,
+        draggable:false,
+        waitForAnimate:true,
+    });
+});
