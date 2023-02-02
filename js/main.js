8df6e8fb89897f4831699d4a8b0e7c1a942bedaa
Oleh <85799629+olehmatskiv@users.noreply.github.com>
2023-02-02 18:07:40 +0200

    finished offering
    Завершив розробку Оферінгу

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+/*$(document).ready(function(){
     $('.slider').slick({
         arrows:true,
         dots:true,
@@ -18,3 +18,32 @@ $(document).ready(function(){
         waitForAnimate:true,
     });
 });
+*/
+
+var slideIndex = 1;
+
+showSlides(slideIndex)
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n)
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("hero__item")
+    if (n > slides.length){
+        slideIndex = 1
+    }
+
+    if (n < 1){
+        slideIndex = slides.length
+    }
+    for (i = 0; i < slides.length; i++){
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex - 1].style.display = "block"
+}
